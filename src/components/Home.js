dd07a652016247b69d8cc714f247c410d9be1c74
Jlindner1 <46451910+Jlindner1@users.noreply.github.com>
2021-05-24 20:47:01 -0600

    text change

--- src/components/Home.js
+++ src/components/Home.js
@@ -144,7 +144,7 @@ class Home extends React.Component {
                         I would love to help you make your ideas and projects a reality! Feel free to look around my site for more info and examples on what I have done/am doing, and if you have any questions or would like to inquire about my services, use the contact form to reach out!
                     </p>
                     <p>
-                        This page is meant to display my current abilities in terms of front-end design, and will evolve as my skills develop further and I increase the breadth of my portfolio. I have designed and implemented this single-page web application from the ground up myself using React.js and my own CSS styling, and I hope this page gives you a good sample of my talents, insight into my skillsets and journey in web development. 
+                        This page is meant to display my current abilities in terms of front-end design, and will evolve as my skills develop further and I increase the breadth of my portfolio. I have designed and implemented this single-page web application from the ground up myself using React.js and my own CSS styling, and I hope this page gives you a good sample of my talents, and insight into my skillsets and journey in web development. 
                     </p>
                 </div>;
         }
