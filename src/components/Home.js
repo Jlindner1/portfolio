6898608e42857703b3cafc6b60d3f5078bd43c51
Jlindner1 <46451910+Jlindner1@users.noreply.github.com>
2021-05-28 23:04:41 -0600

    QoL changes, fancy text animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -137,26 +137,31 @@ class Home extends React.Component {
             content = 
                 <div className="content">
                     <h3>Welcome!</h3>
-                    <p>
-                        I'm Josiah, a software developer specializing in web-based apps and projects, in both Front-end and Back-end capacities. Creating new things is my passion, and learning along the way is just part of the fun!
-                    </p>
-                    <p>
-                        I would love to help you make your ideas and projects a reality! Feel free to look around my site for more info and examples on what I have done/am doing, and if you have any questions or would like to inquire about my services, use the contact form to reach out!
-                    </p>
-                    <p>
-                        This page is meant to display my current abilities in terms of front-end design, and will evolve as my skills develop further and I increase the breadth of my portfolio. I have designed and implemented this single-page web application from the ground up myself using React.js and my own CSS styling, and I hope this gives you a good sample of my talents, and insight into my skillsets and journey in web development. 
-                    </p>
+                    <div className="card">
+                        <div>
+                            <p>
+                                I'm Josiah, a software developer specializing in web-based apps and projects, in both Front-end and Back-end capacities. Creating new things is my passion, and learning along the way is just part of the fun!
+                            </p>
+                            <p>
+                                I would love to help you make your ideas and projects a reality! Feel free to look around my site for more info and examples on what I have done/am doing, and if you have any questions or would like to inquire about my services, use the contact form to reach out!
+                            </p>
+                            <p>
+                                This page is meant to display my current abilities in terms of front-end design, and will evolve as my skills develop further and I increase the breadth of my portfolio. I have designed and implemented this single-page web application from the ground up myself using React.js and my own CSS styling, and I hope this gives you a good sample of my talents, and insight into my skillsets and journey in web development. 
+                            </p>                            
+                        </div>
+                    </div>
                 </div>;
         }
         else if (page === "php-projects") {
             content = 
                 <div className="content">
+                    <a className href="https://cms-messages.herokuapp.com" target="_blank"><h3>Messenger Appplication</h3></a>
                     <div className="card">
                         <div className="img-container">
                             <img src={messagesystem} alt="Messenger App Thumbnail" />
                         </div>
                         <div>
-                            <a className href="https://cms-messages.herokuapp.com" target="_blank"><h3>Messenger Appplication</h3></a>
+                            
                             <p>
                                 An application built with PHP and a codeigniter framework, utilizing IonAuth user management and an interactive database to allow users to sign up/login and send messages to each other. This application is hosted through heroku, and utilizes a cloud hosted MySQL database.
                             </p>
@@ -170,12 +175,13 @@ class Home extends React.Component {
         else if (page === "static") {
             content = 
                 <div className="content cards">
+                    <a className href="https://jlindner1.github.io/responsive-design/" target="_blank"><h3>Responsive Static Web Page</h3></a>
                     <div className="card">
                         <div className="img-container">
                             <img src={responsive_page} alt="responsive website image" />
                         </div>
                         <div>
-                            <a className href="https://jlindner1.github.io/responsive-design/" target="_blank"><h3>Responsive Static Web Page</h3></a>
+                            
                             <p>
                                 A sample showing techniques and implementation of responsive web design principles. This spage is designed utilizing a mobile-first methodology to ensure that it is convenient and easy to use for a majority of the web-using population who is experienceing the internet through a smartphone, as well as desktop users. 
                             </p>
@@ -184,12 +190,13 @@ class Home extends React.Component {
                             </p>
                         </div>
                     </div>
+                    <a className href="https://jlindner1.github.io/storefront-sample/" target="_blank"><h3>Storefront Static Web Page</h3></a>
                     <div className="card">
                         <div className="img-container">
                             <img src={storefront} alt="storefront website image" />
                         </div>
                         <div>
-                            <a className href="https://jlindner1.github.io/storefront-sample/" target="_blank"><h3>Storefront Static Web Page</h3></a>
+                            
                             <p>
                                 A sample webpage showcasing a possible storefront design utilizing dropdowns, and shop item cards. This is not a responsive page, and showcases utilization of CSS design abilities rather than a funcitonal page.
                             </p>
@@ -203,12 +210,13 @@ class Home extends React.Component {
         else if (page === "react") {
             content = 
                 <div className="content">
+                    <a className href="https://jlindner1.github.io/pokedex/" target="_blank"><h3>Pokédex Front-end</h3></a>
                     <div className="card">
                         <div className="img-container">
                             <img src={pokemon} alt="pokemon API page image" />
                         </div>
                         <div>
-                            <a className href="https://jlindner1.github.io/pokedex/" target="_blank"><h3>Pokédex Front-end</h3></a>
+                            
                             <p>
                                 A react application interacting with data from the PokéAPI to create a simple Pokédex.
                                 this project started from a demonstration of how to work with the API, but all the internal logic is mine, and the styling is the only leftover from the demo. These stylings will be replaced and additional functionality will be added as I have time to evolve the project.
@@ -220,12 +228,13 @@ class Home extends React.Component {
         else if (page === "education") {
             content = 
                 <div className="content">
+                    <a className href="#" target="_blank"><h3>Under Construction...</h3></a>
                     <div className="card">
                         <div className="img-container">
                             <img src={construction} alt="under construction image" />
                         </div>
                         <div>
-                            <a className href="#" target="_blank"><h3>Under Construction...</h3></a>
+                            
                             <p>
                                 This page is still being worked on, come back soon!
                             </p>
@@ -236,12 +245,13 @@ class Home extends React.Component {
         else if (page === "personal") {
             content = 
                 <div className="content">
+                    <a className href="#" target="_blank"><h3>Under Construction...</h3></a>
                     <div className="card">
                         <div className="img-container">
                             <img src={construction} alt="under construction image" />
                         </div>
                         <div>
-                            <a className href="#" target="_blank"><h3>Under Construction...</h3></a>
+                            
                             <p>
                                 This page is still being worked on, come back soon!
                             </p>
@@ -252,12 +262,13 @@ class Home extends React.Component {
         else if (page === "client") {
             content = 
                 <div className="content">
-                    <div className="card">
+                    <h3>The Sacred Cow Company</h3>
+                    <div className="card card-col">
                         <div className="img-container">
                             <img className="scc" src={SCC_portrait_logo} alt="Sacred Cow Company logo" />
                         </div>
                         <div>
-                            <h3>The Sacred Cow Company</h3>
+                            
                             <p>
                                 I worked with a team of my fellow students to develop a Minimum Viable Product for The Sacred Cow Company, a business focusing on coaching and developing leadership skills and prepping people for management or leadership positions.
                                 The web app is used to keep track of clients, coaches and their appointments and progress in the curriculum.
@@ -277,13 +288,13 @@ class Home extends React.Component {
         }
         else if (page === "bio") {
             content = 
-                <div className="content">
-                    <div className="card">
+                <div className="content"><h3>About Me</h3>
+                    <div className="card card-col">
                         <div className="img-container">
                             <img src={me} alt="image of author" />
                         </div>
                         <div>
-                            <h3>About Me</h3>
+                            
                             <p>
                                 From Edmonton Alberta, Canada - I am a Graduate from NAIT's DMIT Computer Software Development program, and a passionate developer always looking for the next oppurtunity to further develop my skills and knowledge.
                             </p>
@@ -300,20 +311,26 @@ class Home extends React.Component {
         else if (page === "resume") {
             content = 
                 <div className="content">
+                    <h3>Resume/Skills</h3>
                     <div className="card">
                         <div class="resume">
-                            <h3>Resume/Skills</h3>
+                            
                             <p>
-                                I have experience in an array of languages in frameworks, and always looking to expand my knowledge and abilities, and will keep this updated as my journey as a developer continues! 
+                                I have experience in an array[] of languages and frameworks, and I am always looking to expand my knowledge and abilities. I will also keep this updated as my journey as a developer continues
                                 
                             </p>
                             <p>My current skillsets are as follows:</p>
+
+                            <div className="skill-card">
                             <h4>C#/Frameworks</h4>
                             <ul>
                                 <li>Proficient in C# fundamentals and best practices</li>
                                 <li>ASP.NET Framework - Designing and developing MVC web applications inlcuding reading/writing to various Database configurations</li>
                                 <li>ASP.NET CORE - Creating sturdy REST APIs utilizing Controller/Service/Repository design methodology</li>
                             </ul>
+                            </div>
+
+                            <div className="skill-card">
                             <h4>JavaScript</h4>
                             <ul>
                                 <li>Proficient in JavaScript fundamentals and best practices</li>
@@ -322,6 +339,9 @@ class Home extends React.Component {
                                 <li>React - Experienced in the React framework/library for creating robust web applications with component and state management</li>
                                 <li>Vue.js - Some experience with the Vue.js library</li>
                             </ul>
+                            </div>
+
+                            <div className="skill-card">
                             <h4>php</h4>
                             <ul>
                                 <li>Proficient in fundamentals and best practices of php 7 and higher</li>
@@ -329,6 +349,9 @@ class Home extends React.Component {
                                 <li>CodeIgniter - experience in ustilizing the CodeIgniter framework for developing robust MVC php projects</li>
                                 <li>Wordpress/Joomla/Other CMS' - Some experience in utilizing/cutomizing CMS' templates and functionality</li>
                             </ul>
+                            </div>
+
+                            <div className="skill-card">
                             <h4>Database</h4>
                             <ul>
                                 <li>SQL - Proficient in fundamentals for SQL Database data-normalization, creation, and management</li>
@@ -337,6 +360,9 @@ class Home extends React.Component {
                                 <li>MySQL/php MyAdmin - Experienced in DB management/utilization</li>
                                 <li>NoSQL - Some experience in Firebase and MongoDB no sql databases</li>
                             </ul>
+                            </div>
+
+                            <div className="skill-card">
                             <h4>Other</h4>
                             <ul>
                                 <li>CSS/SASS - Proficient in utilizing CSS/SASS to add beautiful, effective, and efficient styling to web projects</li>
@@ -344,6 +370,8 @@ class Home extends React.Component {
                                 <li>Futter/Dart - Some experience in developing mobile applications with the Flutter framework</li>
                                 <li>Java - Some experience in creating/managing Java projects</li>
                             </ul>
+                            </div>
+
                         </div>
                     </div>
                 </div>;
@@ -351,6 +379,8 @@ class Home extends React.Component {
         else if (page === "contact") {
             content = 
                 <div className="content">
+                    <h3>I'd love to hear from you!</h3>
+                    <p>Send me an email with the form below if you have any questions or would like to inquire about my services</p>
                     <div id="error-msg" className="error-msg fade">
                         {(errors.length > 0) ? errors.map(e => (<h4>{e}</h4>)) : "didnt work"}
                     </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/Sidebar.css';
 import me from '../img/me.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
 
 class Sidebar extends React.Component {
     constructor(props){
@@ -19,6 +21,16 @@ class Sidebar extends React.Component {
                     <h1>Hello!</h1>
                     <h2>Josiah Is:</h2>
                     <p>Developing Web Applications From</p><p className="location-p">Edmonton AB, Canada</p>
+                    <div className="social">
+                        <a className="social-link" href="https://www.linkedin.com/in/josiah-lindner-28b872b6" target="_blank">
+                            <FontAwesomeIcon icon={ faLinkedin } className="icon" />
+                            <h4>linkedin</h4>
+                        </a>
+                        <a className="social-link" href="https://github.com/Jlindner1" target="_blank">
+                            <FontAwesomeIcon icon={ faGithubSquare } className="icon" />
+                            <h4>GitHub</h4>
+                        </a>
+                    </div>
                 </section>                
             </div>
 
